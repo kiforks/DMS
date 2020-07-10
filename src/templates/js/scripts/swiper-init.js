bdeab7249514e636fe455fd9680814313afdac7b
kiforks <ks.kifor@gmail.com>
2020-07-10 17:49:14 +0300

    Add swiper slider
    * Changed swiperjs classes to BEM methodology
    * Started coding 'services' section

---
+++ src/templates/js/scripts/swiper-init.js
@@ -0,0 +1,50 @@
+function initSwiperJS(slider) {
+  if(!slider) {
+    return;
+  }
+
+  let mySwiper = new Swiper(`.${slider.container}`, {
+    pagination: {
+      el: `.${slider.container}__pagination`,
+      type: `fraction`,
+      currentClass: `${slider.container}__current`,
+      totalClass: `${slider.container}__total`,
+    },
+    navigation: {
+      nextEl: `.${slider.container}__button--next`,
+      prevEl: `.${slider.container}__button--prev`,
+    },
+    loop: slider.loop || false,
+    wrapperClass: `${slider.container}__list`,
+    slideClass: `${slider.container}__item`,
+    slideActiveClass: `${slider.container}__item--active`,
+    slideDuplicateActiveClass: `${slider.container}__item-duplicate--active`,
+    slideVisibleClass: `${slider.container}__item--visible`,
+    slideDuplicateClass: `${slider.container}__item--duplicate`,
+    slideNextClass: `${slider.container}__item--next`,
+    slideDuplicateNextClass: `${slider.container}__item-duplicate--next`,
+    slidePrevClass: `${slider.container}__item--prev`,
+    slideDuplicatePrevClass: `${slider.container}__item-duplicate--prev`,
+    slideBlankClass: `${slider.container}__invisible-blank`,
+    bulletClass: `${slider.container}__bullet`,
+    bulletActiveClass: `${slider.container}__bullet--active`,
+    modifierClass: `${slider.container}__pagination`,
+    hiddenClass: `${slider.container}__hidden`,
+    progressbarFillClass: `${slider.container}__progressbar-fill`,
+    clickableClass: `${slider.container}__clickable`,
+    lockClass: `${slider.container}__lock`,
+    progressbarOppositeClass: `${slider.container}__progressbar-opposite`,
+  });
+}
+
+const slider = {
+  container: 'swiper',
+  item: 3,
+  loop: true
+};
+
+initSwiperJS(slider);
+
+
+
+
